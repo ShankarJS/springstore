ecb62564bd2f13ddfa6f83711127b07d2dd7e79f
Shankarlal Sharma <shankarjsharma@gmail.com>
2025-12-03 19:04:42 +0530

    Added react url in cors policy

--- src/main/java/com/shankar/springstore/config/WebConfig.java
+++ src/main/java/com/shankar/springstore/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")   // allow all endpoints
-                .allowedOrigins("http://localhost:4200")  // Angular URL
+                .allowedOrigins("http://localhost:4200", // Angular URL
+                        "http://localhost:3000")  //React URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
